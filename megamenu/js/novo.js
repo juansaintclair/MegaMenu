1b58e971fb66c8de8bbba81049a9385f543b57ff
Ygor Lazaro <ygor@ygorlazaro.com>
2015-05-23 16:58:40 -0300

    Posicionamento do código

--- megamenu/js/novo.js
+++ megamenu/js/novo.js
@@ -6,26 +6,32 @@ var config = {
 
 $(function () {
 	$('.pai').on('mouseenter', function () {
-		var nomePai = $(this).data('nome');
-		var larguraPai = getLargura($(this));
-
-		$('.filho[data-pai="' + nomePai + '"]').show().animate({ marginLeft: larguraPai });
+		getLargura($(this));
 	})
 		.on('mouseleave', function () {
 		setTimeout(function () {
 			$('.filho').hide().css('margin-left', '');
-		}, 10000);
+		}, 1000);
 
 	});
 });
 
 function getLargura($item) {
 	var largura = $item.width();
+	var pai = $item.data('pai');
 
 	if ($item.hasClass('filho')) {
-		var pai = $item.data('pai');
 		largura += getLargura($('[data-nome="' + pai + '"]'));
 	}
+
+	var seletor = pai ? '[data-pai="' + pai + '"]' : '.filho'; 
+
+	$(seletor)
+		.children($item)
+		.show()
+		.animate({
+		marginLeft: largura
+	});
 
 	return largura;
 }
